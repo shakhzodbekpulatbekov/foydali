ebfe840e12b3bb0ccb4ab0951554df1fa710f3a3
shakhzodbekpulatbekov <73734816+shakhzodbekpulatbekov@users.noreply.github.com>
2023-01-07 11:42:44 +0500

    target

---
+++ src/main/java/pdp_g9/telegram_bot/service/priceService/PriceService.java
@@ -0,0 +1,42 @@
+package pdp_g9.telegram_bot.service.priceService;
+
+import org.springframework.stereotype.Service;
+import pdp_g9.telegram_bot.entity.price.PriceEntity;
+import pdp_g9.telegram_bot.repository.price.PriceRepository;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+@Service
+public class PriceService {
+    final PriceRepository priceRepository;
+
+    public PriceService(PriceRepository priceRepository) {
+        this.priceRepository = priceRepository;
+    }
+
+    public void WriteToFile(){
+        List<PriceEntity> all = priceRepository.findAll();
+        byte[] demBytes = all.get(0).getPhotoByte();
+
+        File outputFile = new File("src/main/resources/price.jpg");
+
+        FileOutputStream outputStream;
+
+        {
+            try {
+                outputStream = new FileOutputStream(outputFile);
+                outputStream.write(demBytes);
+            } catch (FileNotFoundException fileNotFoundException) {
+                fileNotFoundException.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+}
