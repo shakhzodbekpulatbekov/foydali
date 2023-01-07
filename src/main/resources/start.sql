8a0d9fd68a1304704afd2f29aa39a3b376fb7a06
shakhzodbekpulatbekov <73734816+shakhzodbekpulatbekov@users.noreply.github.com>
2023-01-07 10:50:46 +0500

    target

--- src/main/resources/start.sql
+++ src/main/resources/start.sql
@@ -1,18 +1,18 @@
-INSERT INTO category_data_base(name, parent_id) VALUES ('O''zbek milliy ta''omlari',0);
-INSERT INTO category_data_base(name, parent_id) VALUES ('Uyg''ur ta''omlari',0);
-INSERT INTO category_data_base(name, parent_id) VALUES ('Yevropa ta''omlari',0);
-INSERT INTO category_data_base(name, parent_id) VALUES ('Tortlar',0);
-INSERT INTO category_data_base(name, parent_id) VALUES ('Pishiriqlar',0);
-INSERT INTO category_data_base(name, parent_id) VALUES ('Salatlar',0);
-INSERT INTO category_data_base(name, parent_id) VALUES ('Quyuq ovqatlar',1);
-INSERT INTO category_data_base(name, parent_id) VALUES ('Suyuq ovqatlar',1);
-INSERT INTO meal_data_base(name, description, category_id, photo_url, video_url) VALUES ('To''y oshi','
-1 kilo qo''y go''shti
-300 gramm dumba yog''i
-1.2 kilo sabzi
-1 kilo guruch
-2 bosh sarimsoqpiyoz
-300 gramm piyoz
-50 gramm tuz
-15 gramm zira
-800-1000 millilitr suv',8,'C:\IdeaProjects\Spring\telegram_bot\photos\to'' oshi.jpg', null)
+-- INSERT INTO category_data_base(name, parent_id) VALUES ('O''zbek milliy ta''omlari',0);
+-- INSERT INTO category_data_base(name, parent_id) VALUES ('Uyg''ur ta''omlari',0);
+-- INSERT INTO category_data_base(name, parent_id) VALUES ('Yevropa ta''omlari',0);
+-- INSERT INTO category_data_base(name, parent_id) VALUES ('Tortlar',0);
+-- INSERT INTO category_data_base(name, parent_id) VALUES ('Pishiriqlar',0);
+-- INSERT INTO category_data_base(name, parent_id) VALUES ('Salatlar',0);
+-- INSERT INTO category_data_base(name, parent_id) VALUES ('Quyuq ovqatlar',1);
+-- INSERT INTO category_data_base(name, parent_id) VALUES ('Suyuq ovqatlar',1);
+-- INSERT INTO meal_data_base(name, description, category_id, photo_url, video_url) VALUES ('To''y oshi','
+-- 1 kilo qo''y go''shti
+-- 300 gramm dumba yog''i
+-- 1.2 kilo sabzi
+-- 1 kilo guruch
+-- 2 bosh sarimsoqpiyoz
+-- 300 gramm piyoz
+-- 50 gramm tuz
+-- 15 gramm zira
+-- 800-1000 millilitr suv',8,'C:\IdeaProjects\Spring\telegram_bot\photos\to'' oshi.jpg', null)
